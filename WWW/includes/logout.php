86ce0858b768e97ff6cc08374aced85f4155bac2
HCAWN <henrycawneilson@gmail.com>
2018-06-14 14:41:26 +0100

    new file structure for fresh deployment

--- WWW/includes/logout.php
+++ WWW/includes/logout.php
@@ -1,22 +1,4 @@
 <?php
-
-/*
- * Copyright (C) 2013 peredur.net
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 include_once 'functions.php';
 sec_session_start();
 
@@ -31,5 +13,5 @@ setcookie(session_name(),'', time() - 42000, $params["path"], $params["domain"],
 
 // Destroy session 
 session_destroy();
-header("Location: ../index");
+header('Location: https://'.$_SERVER['SERVER_NAME']);
 exit();
--- WWW/includes/process_login.php
+++ WWW/includes/process_login.php
@@ -1,26 +1,7 @@
 <?php
-
-/*
- * Copyright (C) 2013 peredur.net
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 include_once 'db_connect.php';
 include_once 'functions.php';
-
-sec_session_start(); // Our custom secure way of starting a PHP session.
+sec_session_start();
 
 if (isset($_POST['email'], $_POST['p'])) {
     $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
@@ -28,7 +9,8 @@ if (isset($_POST['email'], $_POST['p'])) {
     
     if (login($email, $password, $mysqli) == true) {
         // Login success 
-        header("Location: ../diary");
+        $_SESSION['encryptstring'] = $_POST['p'];
+        header('Location: https://'.$_SERVER['SERVER_NAME']);
         exit();
     } else {
         // Login failed 
