ced309f5d308eda5802ec4b07d9b28a4a6bc92e4
hcawn <henrycawneilson@gmail.com>
2018-07-11 14:52:33 -0700

    resolving

--- includes/psl-config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("HOST", ""); 			// The host you want to connect to. 
-define("USER", ""); 			// The database username. 
-define("PASSWORD", ""); 	// The database password. 
-define("DATABASE", "");   // The database name.
-
-define("CAN_REGISTER", "any");
-define("DEFAULT_ROLE", "member");
-define("SECURE", FALSE);
