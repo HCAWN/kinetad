50bd0b9499b9a6a9d9782ec159d159ddbdd19df7
Henry Neilson <40216444+HCAWN@users.noreply.github.com>
2018-07-11 23:01:06 +0100

    new blank psl-config.php

---
+++ includes/psl-config.php
@@ -0,0 +1,6 @@
+<?php
+define("HOST", "localhost"); 			// The host you want to connect to. 
+define("USER", "DiaryEnter"); 			// The database username. 
+define("PASSWORD", "password"); 	// The database password. 
+define("DATABASE", "KinetadDiary");             // The database name.
+?>
