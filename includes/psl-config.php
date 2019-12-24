08ef7c0125e5f241a8e2b1989a1175ab9ad2272f
HCAWN <henrycawneilson@gmail.com>
2019-12-24 21:53:44 +0000

    move all into source directory

--- includes/psl-config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("HOST", "localhost"); 			// The host you want to connect to. 
-define("USER", "DiaryEnter"); 			// The database username. 
-define("PASSWORD", "password"); 	// The database password. 
-define("DATABASE", "KinetadDiary");             // The database name.
-?>
